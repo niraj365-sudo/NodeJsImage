0a7da0486695d92dc1b4c4fb9befdaa3fc21a6a4
Niraj Paudel <poudelniraj095@gmail.com>
2022-12-25 02:49:52 +0545

    Twak

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ dotenv.config()
 
 
 //Database connection
+mongoose.set("strictQuery", false);
 mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
--- handler/cloudinary.js
+++ handler/cloudinary.js
@@ -1,9 +1,9 @@
-const cloudinary = require("cloudinary");
-  cloudinary.config({
-  cloud_name: process.env.CLOUD_NAME,
-  api_key: process.env.API_ID,
-  api_secret: process.env.API_SECRET,
-});
+// const cloudinary = require("cloudinary");
+//   cloudinary.config({
+//   cloud_name: process.env.CLOUD_NAME,
+//   api_key: process.env.API_ID,
+//   api_secret: process.env.API_SECRET,
+// });
 
 
 
